a7b84a44d28463d8389f8a0daa4ba1f1e76a7a17
Ardikaoktavian <114373734+Ardikaoktavian@users.noreply.github.com>
2023-01-08 18:49:13 +0700

    praktikum 4-inframe & layout

---
+++ P4_ardika_TI05/tugas4_ardika_ti05/tugas4.py
@@ -0,0 +1,20 @@
+namaLengkapSaya="Nama Lengkap : Ardika Oktavian"
+namaPanggilanSaya="Nama Panggilan : Ardika,Dika,Dik"
+TempatTanggalLahir="Tempat Tanggal Lahir : Depok, 14 Oktober 2003"
+hobiSaya="Hobi : Bulutangkis"
+DomisiliSekarang="Domisili : Jln. Margonda Raya"
+no_rombel="No Rombel : TI05"
+no_nim="No Nim : 0110222151"
+prodi_kuliah="Prodi Kuliah :  Teknik Informatika"
+
+print(namaLengkapSaya,type(namaLengkapSaya))
+print(namaPanggilanSaya,type(namaPanggilanSaya))
+print(TempatTanggalLahir,type(TempatTanggalLahir))
+print(hobiSaya,type(hobiSaya))
+print(DomisiliSekarang,type(DomisiliSekarang))
+print(no_rombel,type(no_rombel))
+print(no_nim,type(no_nim))
+print(prodi_kuliah,type(prodi_kuliah))
+print(8>7)
+print(8==8)
+print(8<4)
